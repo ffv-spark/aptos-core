4e9c0e945cf9e95b0111108091d18a9d61fef3d6
Zekun Li <zekunli@Zekuns-MacBook-Pro.local>
2025-11-04 16:35:59 -0800

    [vm] avoid cloning metadata

--- third_party/move/move-vm/runtime/src/storage/loader/eager.rs
+++ third_party/move/move-vm/runtime/src/storage/loader/eager.rs
@@ -19,11 +19,11 @@ use move_binary_format::{
     access::ScriptAccess,
     errors::{PartialVMError, PartialVMResult, VMResult},
     file_format::CompiledScript,
+    CompiledModule,
 };
 use move_core_types::{
     identifier::IdentStr,
     language_storage::{ModuleId, TypeTag},
-    metadata::Metadata,
     vm_status::StatusCode,
 };
 use move_vm_types::{
@@ -242,11 +242,11 @@ where
         _gas_meter: &mut impl DependencyGasMeter,
         _traversal_context: &mut TraversalContext,
         module_id: &ModuleId,
-    ) -> PartialVMResult<Vec<Metadata>> {
+    ) -> PartialVMResult<Arc<CompiledModule>> {
         // Note:
         //   For backwards compatibility, metadata accesses were never metered.
         self.module_storage
-            .unmetered_get_existing_module_metadata(module_id.address(), module_id.name())
+            .unmetered_get_existing_deserialized_module(module_id.address(), module_id.name())
             .map_err(|err| err.to_partial())
     }
 }
